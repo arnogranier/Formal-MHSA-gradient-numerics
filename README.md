e4e369a421c9333fb45ee30db2b2e8519c52fb28
Arno Granier <arno.granier@unibe.ch>
2025-03-16 18:58:12 +0100

    add readme, reorganize

---
+++ README.md
@@ -0,0 +1,11 @@
+This code supports the preprint [preprintlink], in particular section [sectionnumber]. This code checks the derived formal gradient of a tokenwise mean squared error loss backpropagated through one multihead linear self-attention block against numerical approximations based on finite differences and forward automatic differentiation. 
+
+The results can be replicated by first installing [julia]("https://julialang.org/downloads/") if necessary, and the necessary packages
+```console
+julia -e 'using Pkg; Pkg.add.(["Random", "ForwardDiff", "LinearAlgebra"])'
+```
+and launching the single script
+```console
+cd numerics
+julia numerics.jl
+```
--- numerics.jl
+++ numerics.jl
@@ -1,5 +1,8 @@
 using Random;
+using ForwardDiff;
 using LinearAlgebra;
+
+Random.seed!(12345);
 
 x = [2*rand(10) .- 1 for i in 1:10];
 y = [2*rand(10) .- 1 for i in 1:10];
@@ -17,17 +20,12 @@ v(i,h) = Wv[h,:,:] * x[i];
 Z(i,h) = sum([dot(ϕ.(q(i,h)), ϕ.(k(j,h))) for j in 1:10]);
 α(i,j,h) = dot(ϕ.(q(i,h)),ϕ.(k(j,h))) / Z(i,h);
 vtilde(i,h) = sum([α(i,j,h)*v(j,h) for j in 1:10]);
-Δ(i,j,h) = Wo[h,:,:]'*e0[i]*x[j]';
+Δ(i,j,h) = Wo[h,:,:]'*e(i)*x[j]';
 Γq(i,j,jprime,h) = (dϕ.(q(i,h)) .* ϕ.(k(jprime, h)))*v(j,h)'*Δ(i,i,h);
 Γk(i,j,jprime,h) = (ϕ.(q(i,h)) .* dϕ.(k(jprime, h)))*v(j,h)'*Δ(i,jprime,h);
-e(y) = 0.5*[y[i] - sum([Wo[h,:,:]*vtilde(i,h) for h in 1:10]) for i in 1:10]
+e(i) = 0.5*(y[i] - sum([Wo[h,:,:]*vtilde(i,h) for h in 1:10]));
 
-function E(y)
-    p = 0.5*[y[i] - sum([Wo[h,:,:]*vtilde(i,h) for h in 1:10]) for i in 1:10]
-    sum(dot(t,t) for t in p)
-end;
-
-function E_explicit(y, x, Wo, Wv, Wk, Wq)
+function E(y, x, Wo, Wv, Wk, Wq)
   xtilde = [zeros(Real,10) for i in 1:10];
   for i in 1:10
     for h in 1:10
@@ -45,52 +43,45 @@ function E_explicit(y, x, Wo, Wv, Wk, Wq)
   end
   p = 0.5*[y[i] - xtilde[i] for i in 1:10]
   return sum(dot(t,t) for t in p)
-end
+end;
 
-E(y)
-E_explicit(y,x,Wo,Wv,Wk,Wq)
+function perturbed(W, ϵ)
+  pW = copy(W)
+  pW[h,1,1] += ϵ
+  return pW
+end;
 
 h = 1;
 ϵ = 1e-7;
 
-E0, e0 = E(y), e(y);
-Wo[h,1,1] += ϵ;
-E1 = E(y);
-dnumeric = (E1-E0)/ϵ
-danalytic = -sum([e0[i]*vtilde(i,h)' for i in 1:10])[1,1]
+dnumeric = (E(y,x,perturbed(Wo, ϵ),Wv,Wk,Wq)-E(y,x,Wo,Wv,Wk,Wq))/ϵ
+danalytic = -sum([e(i)*vtilde(i,h)' for i in 1:10])[1,1]
+println("Relative error between finite differences and analytic gradient for one term of W_O : ", abs((danalytic-dnumeric)/dnumeric))
 
-E0, e0 = E(y), e(y);
-Wq[h,1,1] += ϵ;
-E1 = E(y);
-dnumeric = (E1-E0)/ϵ
+dnumeric = (E(y,x,Wo,Wv,Wk,perturbed(Wq,ϵ))-E(y,x,Wo,Wv,Wk,Wq))/ϵ
 danalytic = -sum([(Γq(i,j,j,h) - α(i,j,h)*sum([Γq(i,j,jprime,h) for jprime in 1:10]))/Z(i,h) for j in 1:10 for i in 1:10])[1,1]
+println("Relative error between finite differences and analytic gradient for one term of W_Q : ", abs((danalytic-dnumeric)/dnumeric))
 
-E0, e0 = E(y), e(y);
-Wk[h,1,1] += ϵ;
-E1 = E(y);
-dnumeric = (E1-E0)/ϵ
+dnumeric = (E(y,x,Wo,Wv,perturbed(Wk,ϵ),Wq)-E(y,x,Wo,Wv,Wk,Wq))/ϵ
 danalytic = -sum([(Γk(i,j,j,h) - α(i,j,h)*sum([Γk(i,j,jprime,h) for jprime in 1:10]))/Z(i,h) for j in 1:10 for i in 1:10])[1,1]
+println("Relative error between finite differences and analytic gradient for one term of W_K : ", abs((danalytic-dnumeric)/dnumeric))
 
-E0, e0 = E(y), e(y);
-Wv[h,1,1] += ϵ;
-E1 = E(y);
-dnumeric = (E1-E0)/ϵ
+dnumeric = (E(y,x,Wo,perturbed(Wv,ϵ),Wk,Wq)-E(y,x,Wo,Wv,Wk,Wq))/ϵ
 danalytic = -sum([α(i,j,h)*Δ(i,j,h) for j in 1:10 for i in 1:10])[1,1]
+println("Relative error between finite differences and analytic gradient for one term of W_V : ", abs((danalytic-dnumeric)/dnumeric))
 
-using ForwardDiff
+dauto = ForwardDiff.gradient(Wo->E(y,x,Wo,Wv,Wk,Wq), Wo)
+danalytic = [-sum([e(i)*vtilde(i,h)' for i in 1:10]) for h in 1:10]
+println("Mean absolute error between forward automatic differentiation and analytic gradient for W_O : ", sum(dauto-permutedims(cat(danalytic...,dims=3),(3,1,2)))/length(dauto))
 
-dauto = ForwardDiff.gradient(Wo->E_explicit(y,x,Wo,Wv,Wk,Wq), Wo)
-danalytic = [-sum([e0[i]*vtilde(i,h)' for i in 1:10]) for h in 1:10]
-sum(dauto-permutedims(cat(danalytic...,dims=3),(3,1,2)))/length(dauto)
-
-dauto = ForwardDiff.gradient(Wv->E_explicit(y,x,Wo,Wv,Wk,Wq), Wv)
-danalytic = [-sum([α(i,j,h)*Δ(i,j,h) for j in 1:10 for i in 1:10]) for h in 1:10]
-sum(dauto-permutedims(cat(danalytic...,dims=3),(3,1,2)))/length(dauto)
-
-dauto = ForwardDiff.gradient(Wk->E_explicit(y,x,Wo,Wv,Wk,Wq), Wk)
-danalytic = [-sum([(Γk(i,j,j,h) - α(i,j,h)*sum([Γk(i,j,jprime,h) for jprime in 1:10]))/Z(i,h) for j in 1:10 for i in 1:10]) for h in 1:10]
-sum(dauto-permutedims(cat(danalytic...,dims=3),(3,1,2)))/length(dauto)
-
-dauto = ForwardDiff.gradient(Wq->E_explicit(y,x,Wo,Wv,Wk,Wq), Wq)
+dauto = ForwardDiff.gradient(Wq->E(y,x,Wo,Wv,Wk,Wq), Wq)
 danalytic = [-sum([(Γq(i,j,j,h) - α(i,j,h)*sum([Γq(i,j,jprime,h) for jprime in 1:10]))/Z(i,h) for j in 1:10 for i in 1:10]) for h in 1:10]
-sum(dauto-permutedims(cat(danalytic...,dims=3),(3,1,2)))/length(dauto)
+println("Mean absolute error between forward automatic differentiation and analytic gradient for W_Q : ", sum(dauto-permutedims(cat(danalytic...,dims=3),(3,1,2)))/length(dauto))
+
+dauto = ForwardDiff.gradient(Wk->E(y,x,Wo,Wv,Wk,Wq), Wk)
+danalytic = [-sum([(Γk(i,j,j,h) - α(i,j,h)*sum([Γk(i,j,jprime,h) for jprime in 1:10]))/Z(i,h) for j in 1:10 for i in 1:10]) for h in 1:10]
+println("Mean absolute error between forward automatic differentiation and analytic gradient for W_K : ", sum(dauto-permutedims(cat(danalytic...,dims=3),(3,1,2)))/length(dauto))
+
+dauto = ForwardDiff.gradient(Wv->E(y,x,Wo,Wv,Wk,Wq), Wv)
+danalytic = [-sum([α(i,j,h)*Δ(i,j,h) for j in 1:10 for i in 1:10]) for h in 1:10]
+println("Mean absolute error between forward automatic differentiation and analytic gradient for W_V : ", sum(dauto-permutedims(cat(danalytic...,dims=3),(3,1,2)))/length(dauto))
